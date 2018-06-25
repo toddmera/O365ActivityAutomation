cfd6f72714974c72db3cef1452a4bdbee6e5ec82
toddmera <tkmera@gmail.com>
2018-06-25 14:52:35 -0700

    inital scratch

---
+++ SharePointOnlineAutomation.ps1
@@ -0,0 +1,16 @@
+############################################################
+# Tenant Information
+$tenantName = 'M365x534198'
+
+$tenantPassword = Read-Host "Enter you tenant password"
+
+$AdminSiteName = "https://$tenantName-admin.sharepoint.com/"
+$spolSiteBaseURL = 'sharepoint.com/sites/'
+
+$admin ='Admin@' + $tenantName + '.onmicrosoft.com'
+$pass = ConvertTo-SecureString -String $tenantPassword -AsPlainText -Force
+$AzureSPOLCreds = New-Object System.Management.Automation.PSCredential ($admin, $pass)
+
+Connect-SPOService $AdminSiteName -Credential $AzureSPOLCreds
+
+Get-SPOSite
