a015f2dfdbb0beb2d48fc9d09ca71985e043eddc
Todd Mera (tmera) <todd.mera@quest.com>
2019-08-05 13:57:48 -0700

    changed tenant

--- OnDemandAutomation.ps1
+++ OnDemandAutomation.ps1
@@ -1,6 +1,6 @@
 ############################################################
 # Tenant Information
-$tenantName = 'M365x774467'
+$tenantName = 'M365x313780'
 $adminRoleName = 'Company Administrator'
 $forwardingSMTPEmail = 'SomeAddress@Quest.com'
 $sharedMailboxes = ("Sales Deptartment", "Marketing Department", "Purchasing Department", "'RandD Department'", "HR Department", "Development Department", "Support Department")
