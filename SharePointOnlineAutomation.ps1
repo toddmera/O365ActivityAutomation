6154846af48c2e41886ef73fbad29b62756c9b7a
Todd Mera (tmera) <todd.mera@quest.com>
2019-08-06 10:53:56 -0700

    Added created subsite functions.

--- SharePointOnlineAutomation.ps1
+++ SharePointOnlineAutomation.ps1
@@ -5,6 +5,9 @@ $tenantName = 'M365x109645'
 # $tenantName = "put password here if you like.  You will have to comment out the line below and uncomment this one"
 $tenantPassword = Read-Host "Enter you tenant password"
 
+# Subwebs to create
+$subwebs = ("Product_Research", "Charity", "Carbon_Zero_Project", "Contoso_Softball_Team")
+
 # Number of cycles to pick random admin and perform tasks
 $adminCycles = 50
 
@@ -94,7 +97,6 @@ function Get-RandomSPOUser {
 
 }
 
-
 function Start-RandomSPOActivity {
 
 
@@ -104,4 +106,53 @@ function Start-RandomSPOActivity {
 
 }
 
+function Add-NewSubWeb {
+
+    #Get a random site name from $subwebs
+    $randomSubWeb = Get-Random $subwebs
+
+    Write-Host "####################################################"
+    Write-Host "#"
+    Write-Host "# New random subweb is $randomSubWeb    "
+    Write-Host "#"
+    Write-Host "####################################################"
+    
+    if (Get-PnPSubWebs -Identity $randomSubWeb.Title) {
+        Write-Host "$randomSubWeb Already exists.  This site will not be created"
+    }else{
+        Write-Host "$randomSubWeb does NOT exists.  Mr. Robot will attempt to create this communications subsite."
+        New-PnPWeb -Title $randomSubWeb -Url $randomSubWeb -Description $randomSubWeb -Locale 1033 -Template "SITEPAGEPUBLISHING#0"
+    }
+        
+}
+
+function Remove-SubWeb {
+
+    #Get a random site name from $subwebs
+    $randomSubWeb = Get-Random $subwebs
+
+    Write-Host "##############################################################################"
+    Write-Host "#"
+    Write-Host "# New random subweb is $randomSubWeb    "
+    Write-Host "#"
+    Write-Host "##############################################################################"
+    
+    if (Get-PnPSubWebs -Identity $randomSubWeb) {
+        Write-Host "##############################################################################"
+        Write-Host "$randomSubWeb exists.  Ms. Pacwoman will remove this site"
+        Write-Host "##############################################################################"
+        Remove-PnPWeb -Url $randomSubWeb -Force
+    }else{
+        Write-Host "##############################################################################"
+        Write-Host "$randomSubWeb does not exist.  So we will just move on.  Deleting it would be like dividing by Zero."
+        Write-Host "##############################################################################"
+    }
+        
+}
+
+############################################################
+# Connect as tenant admin to start the whole thing off.
 InitialConnectionSPO
+
+# Let's make some random stuff happen
+# Start-RandomSPOActivity
