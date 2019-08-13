a2c47d413f6eb37cb90b034f93bb39f8a4d7db2a
Todd Mera (tmera) <todd.mera@quest.com>
2019-08-13 15:44:00 -0600

    .

---
+++ SharepointScratchPad.ps1
@@ -0,0 +1,22 @@
+﻿Install-PnPApp
+
+Get-PnPApp Get-PnPContentType
+
+Get-PnPApp -Scope ToddDocLibApp
+
+Get-PnPList -Includes "Title" | Select-Object title
+
+Get-PnPListItem
+
+$spoAppList = ("Security_Documents", "HR_Policy_and_Procedurs", "Product_Design_docs")
+
+Get-PnPList -Identity ToddDocLibApp
+
+New-PnPList -Title Announcements -Template Announcements
+
+New-PnPList -Title MyContactList -Template Contacts
+
+Add-PnPListItem -List "MyContactList" -Values @{"Title" = "Dixon"; "FirstName"="Bowen"}
+
+$boo = Get-PnPListItem -List MyContactList -Id 2 
+Get-PnPField -List MyContactList
